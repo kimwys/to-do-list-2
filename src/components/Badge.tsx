559975a3ce39c20f531a2924679826e6b9caae7b
Quy Nguyen <ntkquy238@gmail.com>
2026-07-29 15:43:02 +0700

    Edit badge component  size

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -11,8 +11,8 @@ type BadgeProps = {
 export default function Badge({ variant }: BadgeProps) {
   return (
     <div
-      className={`rounded-[1187.5px] px-[19px] py-[9.5px] h-[42px] 
-        font-bold text-background text-[19px]] ${variants[variant]}`}
+      className={`rounded-[1187.5px] px-[19px] py-[9.5px] h-[42px] min-w-[94.91px]
+        text-center font-bold text-background text-[19px]] ${variants[variant]}`}
     >
       {variant}
     </div>
